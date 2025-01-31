25826bdfcd105f794f984fc65af98c6501c9523c
hikaric <minhquang200257@gmail.com>
2025-01-31 11:27:56 +0700

    Update list.vue

--- pages/common/building/list.vue
+++ pages/common/building/list.vue
@@ -10,13 +10,14 @@
         </div>
       </a>
       <div class="actions">
-        <div> <button><svg class="h-8 w-8 text-neutral-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <div class="update"> <button><svg class="h-8 w-8 text-neutral-500" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
             </svg>
           </button>
         </div>
-        <div>
+        <div class="delete">
           <button><svg class="h-8 w-8 text-neutral-500" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
               stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" />
@@ -72,11 +73,20 @@ useHead({
 }
 
 .actions {
-  display: flexbox;
+  display: flex;
   justify-content: center;
+  align-items: center;
 }
 
 .actions :hover {
   color: black;
+}
+
+.delete {
+  margin-left: 20%;
+}
+
+.update {
+  margin-right: 20%;
 }
 </style>

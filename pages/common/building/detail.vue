23681850e5ecf8c6b9c8e445afaf40266c0abc9c
hikaric <minhquang200257@gmail.com>
2025-02-07 10:56:10 +0700

    Update detail.vue

--- pages/common/building/detail.vue
+++ pages/common/building/detail.vue
@@ -1,27 +1,32 @@
 <template>
     <div class="card flex items-center justify-around">
-        <form>
-            <div class="form_group">
-                <label for="building_id">Building ID</label><br>
-                <input type="text" id="building_id" name="buidling_id" value="A01-01" readonly><br>
-                <label for="building_address">Address</label><br>
-                <textarea id="building_address"
-                    name="building_address">144 Tran Duy Phuong, Binh Hung Hoa, Binh Tan</textarea><br>
-                <label for="building_name">Building Name</label><br>
-                <input type="text" id="building_name" name="buidling_name" value="A01-01" readonly><br>
-                <label for="building_floors">Number of Floors</label><br>
-                <input type="text" id="building_floors" name="buidling_floors" value="12" readonly><br>
-                <label for="building_rooms">Number of Rooms</label><br>
-                <input type="text" id="building_rooms" name="buidling_rooms" value="72" readonly><br>
-                <label for="contracted_rooms">Number of Contracted Rooms</label><br>
-                <input type="text" id="contracted_rooms" name="contracted_rooms" value="32" readonly><br>
-                <label for="paid_rooms">Number of Paid Rooms</label><br>
-                <input type="text" id="paid_rooms" name="Paid_rooms" value="32" readonly><br>
-                <label for="unpaid_rooms">Number of Unpaid Rooms</label><br>
-                <input type="text" id="unpaid_rooms" name="unpaid_rooms" value="0" readonly><br>
-            </div>
+        <div class="row flex items-center justify-around">
+            <form>
+                <div class="column">
+                    <label for="building_id">Building ID</label><br>
+                    <input type="text" id="building_id" name="buidling_id" value="A01-01" readonly><br>
+                    <label for="building_address">Address</label><br>
+                    <textarea id="building_address"
+                        name="building_address">144 Tran Duy Phuong, Binh Hung Hoa, Binh Tan</textarea><br>
+                    <label for="building_name">Building Name</label><br>
+                    <input type="text" id="building_name" name="buidling_name" value="A01-01" readonly><br>
+                    <label for="building_floors">Number of Floors</label><br>
+                    <input type="text" id="building_floors" name="buidling_floors" value="12" readonly><br>
 
-        </form>
+                </div>
+                <div class="column">
+                    <label for="building_rooms">Number of Rooms</label><br>
+                    <input type="text" id="building_rooms" name="buidling_rooms" value="72" readonly><br>
+                    <label for="contracted_rooms">Number of Contracted Rooms</label><br>
+                    <input type="text" id="contracted_rooms" name="contracted_rooms" value="32" readonly><br>
+                    <label for="paid_rooms">Number of Paid Rooms</label><br>
+                    <input type="text" id="paid_rooms" name="Paid_rooms" value="32" readonly><br>
+                    <label for="unpaid_rooms">Number of Unpaid Rooms</label><br>
+                    <input type="text" id="unpaid_rooms" name="unpaid_rooms" value="0" readonly><br>
+                </div>
+
+            </form>
+        </div>
         <div class="pic">
             <img src="/image/building.jpg">
         </div>
@@ -66,9 +71,23 @@ useHead({
     flex-wrap: wrap;
 }
 
-.form_group {
-    flex: 1 1 45%;
-    margin: 10px;
+/* .column {
+    float: left;
+    flex: 50%;
+}
+
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+} */
+
+.row {
+    display: flex;
+}
+
+.column {
+    flex: 50%;
 }
 
 input {
@@ -86,6 +105,6 @@ textarea {
 }
 
 .pic {
-    max-width: 25%;
+    max-width: 20%;
 }
 </style>

1b86e71546b9f6b6fc4d48351c21a54b73a29c77
hikaric <minhquang200257@gmail.com>
2025-02-07 09:11:33 +0700

    Update detail.vue

--- pages/common/building/detail.vue
+++ pages/common/building/detail.vue
@@ -3,6 +3,21 @@
         <form>
             <label for="building_id">Building ID</label><br>
             <input type="text" id="building_id" name="buidling_id" value="A01-01" readonly><br>
+            <label for="building_address">Address</label><br>
+            <input type="text" id="building_address" name="buidling_address"
+                value="144 Tran Duy Phuong, Binh Hung Hoa, Binh Tan" readonly><br>
+            <label for="building_name">Building Name</label><br>
+            <input type="text" id="building_name" name="buidling_name" value="A01-01" readonly><br>
+            <label for="building_floors">Number of Floors</label><br>
+            <input type="text" id="building_floors" name="buidling_floors" value="12" readonly><br>
+            <label for="building_rooms">Number of Rooms</label><br>
+            <input type="text" id="building_rooms" name="buidling_rooms" value="72" readonly><br>
+            <label for="contracted_rooms">Number of Contracted Rooms</label><br>
+            <input type="text" id="contracted_rooms" name="contracted_rooms" value="32" readonly><br>
+            <label for="paid_rooms">Number of Paid Rooms</label><br>
+            <input type="text" id="paid_rooms" name="Paid_rooms" value="32" readonly><br>
+            <label for="unpaid_rooms">Number of Unpaid Rooms</label><br>
+            <input type="text" id="unpaid_rooms" name="unpaid_rooms" value="0" readonly><br>
         </form>
     </div>
 </template>
@@ -26,4 +41,8 @@ useHead({
 });
 </script>
 
-<style></style>
+<style>
+input{
+    width:fit-content;
+}
+</style>

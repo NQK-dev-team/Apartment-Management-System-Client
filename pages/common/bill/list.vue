04bf8307343c4d880840afbfef09999717313608
hikaric <minhquang200257@gmail.com>
2025-02-25 09:59:35 +0700

    upload bill list ui files

---
+++ pages/common/bill/list.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="px-4 mt-3 py-3" :class="[lightMode ? 'bg-[#ffffff]' : 'bg-[#1f1f1f] text-white']">
+            <a-breadcrumb>
+                <a-breadcrumb-item>{{ $t('bill_list') }}</a-breadcrumb-item>
+            </a-breadcrumb>
+            <h1 class="mt-3 text-2xl">{{ $t('bill_list') }}</h1>
+            <div class="flex justify-center">
+                <a-input-search class="w-[500px]" v-model:value="searchValue" :placeholder="$t('enter_search')"
+                    enter-button />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { getMessageCode } from '~/consts/api_response';
+import { api } from '~/services/api';
+import { ref } from 'vue';
+
+// ---------------------- Metadata ----------------------
+definePageMeta({
+    name: 'Bill List',
+    layout: 'main',
+    middleware: ['authorization-manager']
+});
+
+useHead({
+    title: 'Bill List',
+    meta: [
+        {
+            name: 'description',
+            content: 'List of bills in the system',
+        },
+    ],
+});
+
+// ---------------------- Variables ----------------------
+// const buildingList = ref<{
+//     name: string;
+//     address: string;
+//     totalRoom: number;
+//     totalFloor: number;
+//     image: string;
+// }[]>([]);
+const { $event } = useNuxtApp();
+const { t } = useI18n();
+const lightModeCookie = useCookie('lightMode');
+const lightMode = computed(
+    () => lightModeCookie.value === null || lightModeCookie.value === undefined || parseInt(lightModeCookie.value) === 1
+);
+const current = ref(1);
+const searchValue = ref("");
+</script>

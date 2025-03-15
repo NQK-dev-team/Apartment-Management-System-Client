bbd49f0eeb3577cf2ba3be8118a370220e1fcccb
KhoaLiang <akhoa2020@gmail.com>
2025-03-15 23:27:39 +0700

    update staffDetail

--- pages/common/staff/detailStaff.vue
+++ pages/common/staff/detailStaff.vue
@@ -18,162 +18,161 @@
     <div class="px-4 py-3 mt-5 overflow-auto" :class="[lightMode ? 'bg-[#ffffff]' : 'bg-[#1f1f1f] text-white']">
       <!-- Heading of the  page -->
       <!-- <h1 class="flex justify-center mt-3 text-2xl ">{{ $t('employee_list') }}</h1> -->
-      <!-- 2 colums -->
-
-        <div class="flex overflow-clip">
-          <!-- Input boxes -->
-          <div class="w-96 flex-1 flex flex-col me-6 border-r border-gray-300">
-            <div class="flex items-center">
-              <div class="flex-1 me-2">
-                <label for="last_name" class="flex mb-1">
-                  <span>{{ $t('last_name') }}</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-input
-                  id="last_name"
-                  value="Nguyễn"
-                  placeholder="Last name"
-                  disabled
-                  readonly
-                />
-              </div>
-              <div class="flex-1 me-2">
-                <label for="name" class="flex mb-1">
-                  <span>{{ $t('middle_name') }}</span>
-                </label>
-                <a-input
-                  id="middle_name"
-                  value="Văn"
-                  placeholder="Middle name"
-                  disabled
-                  readonly
-                />
-              </div>
+      <!-- 2 columns -->
+      <div class="flex overflow-x-auto">
+        <!-- Input boxes -->
+        <div class="flex-1 flex flex-col me-6">
+          <div class="flex items-center">
+            <div class="flex-1 me-2">
+              <label for="last_name" class="flex mb-1">
+                <span>{{ $t('last_name') }}</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-input
+                id="last_name"
+                value="Nguyễn"
+                placeholder="Last name"
+                disabled
+                readonly
+              />
             </div>
-            <div class="flex items-center mt-5">
-              <div class="flex-1 me-2">
-                <label for="name" class="flex mb-1">
-                  <span>{{ $t('name') }}</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-input
-                  id="name"
-                  value="Tùng"
-                  placeholder="Name"
-                  disabled
-                  readonly
-                />
-              </div>
-              <div class="flex-1 me-2">
-                <label for="data_of_birth" class="flex mb-1">
-                  <span>{{ $t('dob') }}</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-date-picker v-model:value="value1" class="w-full" placeholder="2000-05-08" disabled readonly/>
-              </div>
-            </div>
-            <div class="flex items-center mt-5">
-              <div class="flex-1 me-2">
-                <label for="gender" class="flex mb-1">
-                  <span>{{ $t('gender') }}</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-select
-                  ref="select"
-                  v-model:value="value2"
-                  style="width: 120px"
-                  :options="options2"
-                  @focus="focus"
-                  @change="handleChange"
-                  class="w-full"
-                  disabled
-                  readonly
-                ></a-select>
-              </div>
-              <div class="flex-1 me-2">
-                <label for="CCCD" class="flex mb-1">
-                  <span>CCCD</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-input
-                  id="cccd"
-                  value="09220000XXXX"
-                  placeholder="CCCD"
-                  disabled
-                  readonly
-                />
-              </div>
-            </div>
-            <div class="flex items-center mt-5">
-              <div class="flex-1 me-2">
-                <label for="CMND" class="flex mb-1">
-                  <span>CMND</span>
-                </label>
-                <a-input
-                  id="cmnd"
-                  value="Empty"
-                  placeholder="CMND"
-                  disabled
-                  readonly
-                />
-              </div>
-              <div class="flex-1 me-2">
-                <label for="origin" class="flex mb-1">
-                  <span>{{ $t('origin') }}</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-input
-                  id="origin"
-                  value="Đồng Nai"
-                  placeholder="Origin"
-                  disabled
-                  readonly
-                />
-              </div>
-            </div>
-            <div class="flex items-center mt-5">
-              <div class="flex-1 me-2">
-                <label for="phone_number" class="flex mb-1">
-                  <span>{{ $t('phone') }}</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-input
-                  id="phone_number"
-                  value="093275XXXX"
-                  placeholder="Phone number"
-                  disabled
-                  readonly
-                />
-              </div>
-              <div class="flex-1 me-2">
-                <label for="login_email" class="flex mb-1">
-                  <span>{{ $t('email') }}</span>
-                  <span class="text-red-500">*</span>
-                </label>
-                <a-input
-                  id="login_email"
-                  value="testmail1@gmail.com"
-                  placeholder="testmail1@gmail.com"
-                  disabled
-                  readonly
-                />
-              </div>
+            <div class="flex-1 me-2">
+              <label for="name" class="flex mb-1">
+                <span>{{ $t('middle_name') }}</span>
+              </label>
+              <a-input
+                id="middle_name"
+                value="Văn"
+                placeholder="Middle name"
+                disabled
+                readonly
+              />
             </div>
           </div>
-          <!-- Images -->
-          <div class="flex flex-col border-2 border-gray-300 w-72 h-72">
-            <label for="">Chân dung</label>
-            <img :src="svgPaths.placeholderImage" alt="Add employee" class="" />
+          <div class="flex items-center mt-5">
+            <div class="flex-1 me-2">
+              <label for="name" class="flex mb-1">
+                <span>{{ $t('name') }}</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-input
+                id="name"
+                value="Tùng"
+                placeholder="Name"
+                disabled
+                readonly
+              />
+            </div>
+            <div class="flex-1 me-2">
+              <label for="data_of_birth" class="flex mb-1">
+                <span>{{ $t('dob') }}</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-date-picker v-model:value="value1" class="w-full" placeholder="2000-05-08" disabled readonly/>
+            </div>
           </div>
-          <div class="flex flex-col border-2 border-gray-300 w-72 h-72">
-            <label for="">Chân dung</label>
-            <img :src="svgPaths.placeholderImage" alt="Add employee" class="" />
+          <div class="flex items-center mt-5">
+            <div class="flex-1 me-2">
+              <label for="gender" class="flex mb-1">
+                <span>{{ $t('gender') }}</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-select
+                ref="select"
+                v-model:value="value2"
+                style="width: 120px"
+                :options="options2"
+                @focus="focus"
+                @change="handleChange"
+                class="w-full"
+                disabled
+                readonly
+              ></a-select>
+            </div>
+            <div class="flex-1 me-2">
+              <label for="CCCD" class="flex mb-1">
+                <span>CCCD</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-input
+                id="cccd"
+                value="09220000XXXX"
+                placeholder="CCCD"
+                disabled
+                readonly
+              />
+            </div>
           </div>
-          <div class="flex flex-col border-2 border-gray-300 w-72 h-72">
-            <label for="">Chân dung</label>
-            <img :src="svgPaths.placeholderImage" alt="Add employee" class="" />
+          <div class="flex items-center mt-5">
+            <div class="flex-1 me-2">
+              <label for="CMND" class="flex mb-1">
+                <span>CMND</span>
+              </label>
+              <a-input
+                id="cmnd"
+                value="Empty"
+                placeholder="CMND"
+                disabled
+                readonly
+              />
+            </div>
+            <div class="flex-1 me-2">
+              <label for="origin" class="flex mb-1">
+                <span>{{ $t('origin') }}</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-input
+                id="origin"
+                value="Đồng Nai"
+                placeholder="Origin"
+                disabled
+                readonly
+              />
+            </div>
+          </div>
+          <div class="flex items-center mt-5">
+            <div class="flex-1 me-2">
+              <label for="phone_number" class="flex mb-1">
+                <span>{{ $t('phone') }}</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-input
+                id="phone_number"
+                value="093275XXXX"
+                placeholder="Phone number"
+                disabled
+                readonly
+              />
+            </div>
+            <div class="flex-1 me-2">
+              <label for="login_email" class="flex mb-1">
+                <span>{{ $t('email') }}</span>
+                <span class="text-red-500">*</span>
+              </label>
+              <a-input
+                id="login_email"
+                value="testmail1@gmail.com"
+                placeholder="testmail1@gmail.com"
+                disabled
+                readonly
+              />
+            </div>
           </div>
         </div>
+        <!-- Images -->
+        <div class="flex flex-col w-72 h-72 mx-1">
+          <label for="">{{ $t('avatar') }}</label>
+          <img :src="svgPaths.placeholderImage" alt="Add employee" class="" />
+        </div>
+        <div class="flex flex-col w-72 h-72 mx-1">
+          <label for="">{{ $t('national_id') + ' ' + $t('front_face') }}</label>
+          <img :src="svgPaths.placeholderImage" alt="Add employee" class="" />
+        </div>
+        <div class="flex flex-col w-72 h-72 mx-1">
+          <label for="">{{ $t('national_id') + ' ' + $t('back_face') }}</label>
+          <img :src="svgPaths.placeholderImage" alt="Add employee" class="" />
+        </div>
+      </div>
       <div class="flex flex-col items-center mt-5">
         <a-button class="my-2 w-[100px]">{{ $t('back') }}</a-button>
       </div>

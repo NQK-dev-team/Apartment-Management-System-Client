ec6e1052f6898febb8d93d480ea57db59483afb9
hikaric <minhquang200257@gmail.com>
2025-03-28 09:39:17 +0700

    Update detail.vue

--- pages/common/customer/detail.vue
+++ pages/common/customer/detail.vue
@@ -25,14 +25,14 @@
       <CommonCustomerDetailCustomerInformation />
       <div class="flex justify-between" :class="[lightMode ? 'bg-[#ffffff]' : 'bg-[#1f1f1f] text-white']">
         <h1 class="mt-3 text-2xl">{{ $t('contract_list') }}</h1>
-        <div class="flex">
+        <!-- <div class="flex">
           <a-button type="primary" class="flex items-center justify-center p-0 w-[36px] rounded-none;">
             <img :src="svgPaths.plus" alt="Add contract" class="w-[12px] h-[12px]" />
           </a-button>
           <a-button type="primary" danger class="flex items-center justify-center p-0 w-[36px] rounded-none; ml-2">
             <img :src="svgPaths.delete" alt="Delete contract" class="w-[12px] h-[12px]" />
           </a-button>
-        </div>
+        </div> -->
       </div>
       <CommonCustomerDetailContractList />
     </div>

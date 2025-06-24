e5f6a77dd6cc3b2f294d4b25179710b931e686d1
astr0tas <nghia369ty@gmail.com>
2025-06-24 22:18:06 +0700

    fix minor technical problem (IDK what's going on)

--- pages/common/staff/addStaff.vue
+++ pages/common/staff/addStaff.vue
@@ -591,3 +591,14 @@ onMounted(() => {
   getBuildingList();
 });
 </script>
+
+<style lang="css">
+/* .align_validation_message_middle .ant-form-item-explain-error{
+  text-align: center;
+}
+
+.align_validation_message_start .ant-form-item-explain-error{
+  text-align: start;
+  margin-left:13px;
+} */
+</style>

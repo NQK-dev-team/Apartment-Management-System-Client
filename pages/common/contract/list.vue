00cdc423acaa22f067c335e2f21641207b95f667
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2025-08-05 13:38:26 +0700

    add contract list page for customers

--- pages/common/contract/list.vue
+++ pages/common/contract/list.vue
@@ -1,10 +1,12 @@
 <template>
   <ContractListStaff v-if="userRole?.toString() === roles.owner || userRole?.toString() === roles.manager" />
+  <ContractListCustomer v-else-if="userRole?.toString() === roles.customer" />
 </template>
 
 <script lang="ts" setup>
 import { roles } from '~/consts/roles';
 import ContractListStaff from '~/pages/common/contract/list.staff.vue';
+import ContractListCustomer from '~/pages/common/contract/list.customer.vue';
 
 // ---------------------- Metadata ----------------------
 definePageMeta({

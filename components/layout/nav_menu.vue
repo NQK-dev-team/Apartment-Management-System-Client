a9dfc8ca89d80c06e94069e789da22707606b7e6
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2025-08-19 11:09:51 +0700

    Update nav_menu.vue

--- components/layout/nav_menu.vue
+++ components/layout/nav_menu.vue
@@ -19,7 +19,7 @@
       <LayoutNavItem
         v-if="props.role === roles.owner"
         :href="pageRoutes.common.report.index"
-        :icon="Chart"
+        :icon="House"
         :label="$t('dashboard')"
         :collapse="collapse"
       />
@@ -83,7 +83,6 @@ import Card from '~/public/svg/card.svg';
 import Edit from '~/public/svg/edit.svg';
 import Room from '~/public/svg/room.svg';
 import House from '~/public/svg/house.svg';
-import Chart from '~/public/svg/chart.svg';
 import { pageRoutes } from '~/consts/page_routes';
 import { roles } from '~/consts/roles';
 

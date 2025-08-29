a1b3d728b4bce65bc3beccd144e560e3e3e50cc2
astr0tas <nghia369ty@gmail.com>
2025-08-29 21:30:55 +0700

    minor UI/UX updates

--- pages/common/building/list.vue
+++ pages/common/building/list.vue
@@ -26,7 +26,7 @@
       </div>
     </div>
     <div class="mt-5 overflow-auto p-3" :class="[lightMode ? 'bg-[#ffffff]' : 'bg-[#1f1f1f] text-white']">
-      <div class="flex justify-center items-center">
+      <div v-if="buildingList.length > 0" class="flex justify-center items-center">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           <CommonBuildingListCard
             v-for="(building, index) in buildingList"
@@ -40,6 +40,52 @@
             :images="building.images"
           />
         </div>
+      </div>
+      <div v-else class="w-full h-full flex items-center justify-center">
+        <tbody>
+          <tr class="h-[150px]">
+            <td :colspan="100">
+              <div class="h-full w-full">
+                <div
+                  class="flex flex-col items-center justify-center overflow-hidden"
+                  :style="{ width: `${400 - 2}px` }"
+                >
+                  <svg v-if="lightMode" width="64" height="41" viewBox="0 0 64 41" xmlns="http://www.w3.org/2000/svg">
+                    <g transform="translate(0 1)" fill="none" fill-rule="evenodd">
+                      <ellipse fill="#f5f5f5" cx="32" cy="33" rx="32" ry="7"></ellipse>
+                      <g fill-rule="nonzero" stroke="#d9d9d9">
+                        <path
+                          d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"
+                        ></path>
+                        <path
+                          d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z"
+                          fill="#fafafa"
+                        ></path>
+                      </g>
+                    </g>
+                  </svg>
+                  <svg v-else width="64" height="41" viewBox="0 0 64 41" xmlns="http://www.w3.org/2000/svg">
+                    <g transform="translate(0 1)" fill="none" fill-rule="evenodd">
+                      <ellipse fill="#272727" cx="32" cy="33" rx="32" ry="7"></ellipse>
+                      <g fill-rule="nonzero" stroke="#3e3e3e">
+                        <path
+                          d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"
+                        ></path>
+                        <path
+                          d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z"
+                          fill="#1d1d1d"
+                        ></path>
+                      </g>
+                    </g>
+                  </svg>
+                  <p class="mt-[8px]" :class="[lightMode ? 'text-[#00000040]' : 'text-[#ffffff40]']">
+                    {{ $t('no_data') }}
+                  </p>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </div>
       <div v-if="buildingList.length > 8" class="flex justify-center mt-10 mb-3">
         <a-pagination v-model:current="current" :total="buildingList.length" :default-page-size="8" />

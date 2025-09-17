1fe401d55e55b6f8f1b78edf239cbb6aa5f1295d
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2025-09-17 13:47:56 +0700

    fix 500 error code returned when deleting userRole cookie (#19)
    Co-authored-by: KhoaLiang <akhoa2020@gmail.com>

--- middleware/authorization.manager.ts
+++ middleware/authorization.manager.ts
@@ -2,8 +2,8 @@ import { roles } from '~/consts/roles';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export default defineNuxtRouteMiddleware((to, from) => {
-  // skip middleware on client side entirely
-  if (import.meta.client) return;
+  // skip middleware on server side entirely
+  if (import.meta.server) return;
   const userRoleCookie = useCookie('userRole');
   const userRole: string = userRoleCookie && userRoleCookie.value ? userRoleCookie.value.toString() : '';
 
